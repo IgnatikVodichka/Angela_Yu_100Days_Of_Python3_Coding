23560adad41f24da5ca032cc32d018185ddafafb
IgnatikVodichka <ignat136@gmail.com>
2021-08-01 02:27:23 +0300

    Added day14 higher lower game

--- day14/day14.0.py
+++ day14/day14.0.py
@@ -1,24 +1,53 @@
 import random
+import os
 from art import logo, vs
 from game_data import data
 
 
-def follower_compare():
-    if compare1["follower_count"] > compare2["follower_count"]:
-        return True
+def celebrity_selector():
+    '''Choses 1 random celebrity'''
+    choice = random.choice(data)
+    return choice
+
+
+def follower_compare_correct_answer(A, B):
+    '''Compares followers from two random celebritites'''
+    if A["follower_count"] > B["follower_count"]:
+        return 'A'
     else:
-        return False
+        return 'B'
 
 
-compare1=random.choice(data)
-compare2=random.choice(data)
+def game():
 
-correct_answer = follower_compare()
+    game_over = False
+    score = 0
 
-print(logo)
+    while not game_over:
 
-print(f"Compare A:{compare1["name"]}, {compare1["description"]}, from: {compare1["country"]}")
+        A = celebrity_selector()
+        B = celebrity_selector()
 
-print(vs)
+        print(logo)
+
+        print(f"Compare A: {A['name']}, {A['description']}, from: {A['country']}")
+
+        print(vs)
+
+        print(f"Against B: {B['name']}, {B['description']}, from: {B['country']}")
+
+        user_answer = input("Who has more followers? 'A' or 'B': ")
+
+        os.system("clear")
+
+        if user_answer == follower_compare_correct_answer(A, B):
+            print("Correct! You Guessed it!")
+            score += 1
+            print(f"Your current score: {score}")
+        else:
+            print("Nope!")
+            print(f"Your final score: {score}")
+            game_over = True
 
 
+game()
